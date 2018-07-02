a8c8f22ed5902ac8cfc80a5dbc38a1e84ab4b233
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-07-02 17:03:19 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const filled = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+filled.addEventListener("dragstart", dragStart);
+filled.addEventListener("dragend", dragEnd);
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => this.className = " invisible", 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+for(const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+function dragDrop() {
+  this.className = "empty";
+  this.append(filled);
+}
